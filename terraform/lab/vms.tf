cd9e63996e6151a63a5ba962de4d725079c7f7af
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-12-30 11:54:56 +0000

    Clean new cluster

--- terraform/lab/vms.tf
+++ terraform/lab/vms.tf
@@ -1,15 +1,16 @@
 
-module "cluster" {
-  source            = "./modules/talos-cluster"
-  proxmox_node_name = "sol"
-  iso_file_id       = proxmox_virtual_environment_download_file.talos_nocloud_image2.id
+module "cluster1" {
+  source             = "./modules/talos-cluster"
+  proxmox_node_name  = "sol"
+  talos_cluster_name = "test-base"
+  iso_file_id        = proxmox_virtual_environment_download_file.talos_nocloud_image2.id
 }
 
 output "talosconfig" {
-  value     = module.cluster.talosconfig
+  value     = module.cluster1.talosconfig
   sensitive = true
 }
 output "kubeconfig" {
-  value     = module.cluster.kubeconfig
+  value     = module.cluster1.kubeconfig
   sensitive = true
 }
