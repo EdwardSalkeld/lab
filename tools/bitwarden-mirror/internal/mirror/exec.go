ec2fc2f41af6bd62372927cd0f9e71e4f67644fa
Edward Salkeld <edsalkeld@alcachofa.faith>
2026-06-01 15:27:16 +0100

    Read bw JSON from stdout only

--- tools/bitwarden-mirror/internal/mirror/exec.go
+++ tools/bitwarden-mirror/internal/mirror/exec.go
@@ -1,7 +1,9 @@
 package mirror
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -14,5 +16,16 @@ func (ExecRunner) Run(ctx context.Context, cmd Command) ([]byte, error) {
 	for key, value := range cmd.Env {
 		execCmd.Env = append(execCmd.Env, key+"="+value)
 	}
-	return execCmd.CombinedOutput()
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	execCmd.Stdout = &stdout
+	execCmd.Stderr = &stderr
+	if err := execCmd.Run(); err != nil {
+		if stderr.Len() > 0 {
+			return stdout.Bytes(), fmt.Errorf("%w: %s", err, bytesTrimSpace(stderr.Bytes()))
+		}
+		return stdout.Bytes(), err
+	}
+	return stdout.Bytes(), nil
 }
