580af80b33dae54f8515a604659d883e1542ad39
Billy <billy@users.noreply.github.com>
2026-07-13 21:45:18 +0000

    Create billy user on partridge

--- nixos/hosts/partridge/configuration.nix
+++ nixos/hosts/partridge/configuration.nix
@@ -50,9 +50,16 @@
   users.users.edward.packages = with pkgs; [
     tree
   ];
-  users.users.edward.openssh.authorizedKeys.keys = [
+  users.users.billy = {
+    isNormalUser = true;
+    extraGroups = [
+      "systemd-journal"
+      "wheel"
+    ];
+    openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgAC7g5CoTIOcrTpzDqFylWrcMGJIqOQC2RrYcWQzhD4NTB8Uh5ZHhR0LMfRhFXivIs3TY+bAe4ov7FODCOimL6irSoj6Pd/2La3o3hXGz2u/l1/7sLWxtG3H7k2QCOHacVzZUznJpn4rAGtfq2w8cmF/RNO1kc/ZncaIlh2TZ8f3D5cAEKUV2f7YN40d9MSnXNgg6YRgL91wfWDO7DMuWUi5UTqcH/3NBcJXsrTEQ7TT10ISabIVoLNROoAiORZY83iy1fYSGN3u3tapxV1EhbW9nsluBonW57jPavUuQIrPhsbDdTvqOiWgC/9XqthHejj7r1q0lo+EZqmqK7wSQMXR4aNt8TMGLWwt6GvM0fbUq/RNs1/C9R5wKu+pmo0CHE+J53HXCzkEFxgOMAqk/dhTSF2aOgUOUFfigVHs2EniUtaEDGCoKbM28kB0UbSnJruGGyK9znu9SJuR2dGM0gk35TfPDJPzGZFKK5ofwbUnV0MqO4zCUrRPYHgCwDCgkw== billy@chatting"
-  ];
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
     git
