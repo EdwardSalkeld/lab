bf8dd9ded45e4856cd832b306476538e3c02d6b4
Edward Salkeld <edsalkeld@alcachofa.faith>
2026-09-14 16:40:43 +0100

    Use DHCP for Falcon primary IPv4

--- nixos/hosts/falcon/configuration.nix
+++ nixos/hosts/falcon/configuration.nix
@@ -9,16 +9,14 @@
   networking = {
     hostName = "falcon";
     useDHCP = false;
-    defaultGateway = {
-      address = "172.31.1.1";
-      interface = "eth0";
-    };
     defaultGateway6 = {
       address = "fe80::1";
       interface = "eth0";
     };
     interfaces.eth0 = {
-      ipv4.addresses = [{ address = "91.99.120.43"; prefixLength = 32; }];
+      # Hetzner Cloud assigns and routes the Primary IPv4 over DHCP. Keeping
+      # this dynamic lets a reassigned Primary IP work without a host change.
+      useDHCP = true;
       ipv6.addresses = [{ address = "2a01:4f8:c17:d035::1"; prefixLength = 64; }];
     };
     firewall.allowedTCPPorts = [ 22 80 443 9100 ];
