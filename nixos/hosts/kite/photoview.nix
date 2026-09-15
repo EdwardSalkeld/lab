a47dda3672feeae5ce11b3e958bc4f3ecea53e26
Edward Salkeld <edsalkeld@alcachofa.faith>
2026-09-15 21:24:10 +0100

    Complete PhotoView libheif compatibility fix

--- nixos/hosts/kite/photoview.nix
+++ nixos/hosts/kite/photoview.nix
@@ -11,7 +11,11 @@
           --replace-fail C.enum_heif_compression_format C.heif_compression_format \
           --replace-fail C.enum_heif_chroma C.heif_chroma \
           --replace-fail C.enum_heif_colorspace C.heif_colorspace \
-          --replace-fail C.enum_heif_channel C.heif_channel
+          --replace-fail C.enum_heif_channel C.heif_channel \
+          --replace-fail 'uint32(compression)' 'C.heif_compression_format(compression)' \
+          --replace-fail 'uint32(colorspace)' 'C.heif_colorspace(colorspace)' \
+          --replace-fail 'uint32(chroma)' 'C.heif_chroma(chroma)' \
+          --replace-fail 'uint32(channel)' 'C.heif_channel(channel)'
       '';
     });
     host = "127.0.0.1";
