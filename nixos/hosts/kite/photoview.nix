b0852dc99a5c8ee1b56276a5f2f0f39eca6ab723
Edward Salkeld <edsalkeld@alcachofa.faith>
2026-09-15 21:20:39 +0100

    Fix PhotoView build with libheif 1.23

--- nixos/hosts/kite/photoview.nix
+++ nixos/hosts/kite/photoview.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.photoview = {
     enable = true;
+    package = pkgs.photoview.overrideAttrs (old: {
+      # PhotoView 2.4.0 vendors libheif Go bindings written before libheif 1.23
+      # renamed these CGo enum types to typedefs.
+      preBuild = (old.preBuild or "") + ''
+        substituteInPlace vendor/github.com/strukturag/libheif/go/heif/heif.go \
+          --replace-fail C.enum_heif_compression_format C.heif_compression_format \
+          --replace-fail C.enum_heif_chroma C.heif_chroma \
+          --replace-fail C.enum_heif_colorspace C.heif_colorspace \
+          --replace-fail C.enum_heif_channel C.heif_channel
+      '';
+    });
     host = "127.0.0.1";
     mediaPath = "/data/full/photos/archive";
     settings = {
